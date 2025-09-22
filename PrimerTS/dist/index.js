34a1b9dbd8c18d159391da9c925c509f4218a972
Mario García Luque <garcialuquemario7@gmail.com>
2025-09-22 13:25:13 +0200

    PrimerTS

---
+++ PrimerTS/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    const greeting = `Hello, ${name}!`;
+    const element = document.getElementById("greeting");
+    if (element) {
+        element.innerText = greeting;
+    }
+}
+greet("World");
+//# sourceMappingURL=index.js.map
