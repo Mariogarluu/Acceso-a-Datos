7fde7acc7881b50d1354b600317ddf2fc95261af
Mario García Luque <garcialuquemario7@gmail.com>
2025-09-26 13:36:42 +0200

    Calculadora es un repo

--- Calculadora/dist/index.d.ts
+++ Calculadora/dist/index.d.ts
@@ -1,2 +1,8 @@
-export {};
+declare let input: string;
+declare let operator: string;
+declare let previousInput: string;
+declare function calcDisplay(values: string): void;
+declare function updateScreen(): void;
+declare function deleteLast(): void;
+declare const buttons: NodeListOf<Element>;
 //# sourceMappingURL=index.d.ts.map
